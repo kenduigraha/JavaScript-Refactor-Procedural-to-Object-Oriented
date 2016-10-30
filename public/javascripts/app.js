70b451d18b8e293ac0caa782588d5df60dbb1edd
Your Name <name@example.com>
2016-10-30 14:44:50 +0700

    release 0 : updated to object, next MVC

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,44 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>')
-    })
+  // let dice = new Dice()
 
-  $('#roller button.roll').on('click', function() {
+  Dice.btnAddClicked()
+
+  Dice.btnRollClicked()
+})
+
+class Dice {
+  // constructor(name){
+  //   this.name = name || null
+  // }
+
+  // =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+  // button : Add Dice!
+  //=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+  static btnAddClicked (){
+    $('#roller button.add').on('click', function() {
+      Dice.updateUIDice()
+    })
+  }
+
+  static updateUIDice (){
+    console.log("WAT")
+    return $('.dice').append('<div class="die">0</div>')
+  }
+
+  // =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+  // buttpm : Roll Dice!
+  //=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+  static btnRollClicked (){
+    $('#roller button.roll').on('click', function() {
+      Dice.updateUIDiceValue()
+    })
+  }
+
+  static updateUIDiceValue () {
     $('.die').each(function(k, die) {
       var value = Math.floor((Math.random()*6)+1)
       $(die).text(value)
     })
-  })
-})
+  }
+
+}
